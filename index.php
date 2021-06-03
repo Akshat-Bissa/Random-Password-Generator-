20ef9feb51a514d12f86b6ee0a9ef0ae52380a4c
Akshat bissa <akshatbissa101006@gmail.com>
2021-06-03 14:52:36 +0530

    Random-Password-generator

---
+++ index.php
@@ -0,0 +1,35 @@
+<h1 align="center">Random-Password-Generater</h1>
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+<h2 align="center"> Your password is generated with <br>
+a b c d e f g h i j k l m n o p <br>
+q r s t u v w x y z|AND|1 2 3 4 <br>
+5 6 7 8 9 0 |! @ # $ % ^ &  *</h2>
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+<form align="center" action="index.php">
+
+
+<input type="number" name="digit" id="di_id" min="1" max="3000" required><br>
+
+
+<button>Submit</button>
+
+</form> <br>
+<?php
+ $str="1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@1234567890abcdefghijklmnopqrstuvwxyz?*&^%$#@";
+ $shuffle= str_shuffle($str);
+$l=implode($_GET);
+if($_GET){
+    
+    
+    echo "your Password is: " . substr($shuffle,0,$l);
+
+    
+
+}else{
+    echo substr($shuffle,0,10);
+    
+}
+ 
+
+
+?>
